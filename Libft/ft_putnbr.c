46ffd2834b66451af063e7c865250b6c1ce07556
Flint Hignett <fhignett@f1r1s10.codam.nl>
2019-02-05 17:17:37 +0100

    ?

---
+++ Libft/ft_putnbr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_putnbr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fhignett <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/01/10 16:58:21 by fhignett      #+#    #+#                 */
+/*   Updated: 2019/01/10 16:58:22 by fhignett      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putnbr(int n)
+{
+	unsigned int nb;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		nb = NTP(n);
+	}
+	else
+		nb = n;
+	if (nb < 10)
+		ft_putchar(nb + 48);
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
